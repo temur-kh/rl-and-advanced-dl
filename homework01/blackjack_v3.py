b4c3eb460565957cf4272637f1b5676ee547aa88
Temur <tim_1998@bk.ru>
2020-11-08 22:11:55 +0300

    Solve homework 1

---
+++ homework01/blackjack_v3.py
@@ -0,0 +1,150 @@
+import gym
+from gym import spaces
+from gym.utils import seeding
+
+def cmp(a, b):
+    return float(a > b) - float(a < b)
+
+# 1 = Ace, 2-10 = Number cards, Jack/Queen/King = 10
+global_deck = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10] * 4
+
+
+def usable_ace(hand):  # Does this hand have a usable ace?
+    return 1 in hand and sum(hand) + 10 <= 21
+
+
+def sum_hand(hand):  # Return current hand total
+    if usable_ace(hand):
+        return sum(hand) + 10
+    return sum(hand)
+
+
+def is_bust(hand):  # Is this hand a bust?
+    return sum_hand(hand) > 21
+
+
+def score(hand):  # What is the score of this hand (0 if bust)
+    return 0 if is_bust(hand) else sum_hand(hand)
+
+
+def is_natural(hand):  # Is this hand a natural blackjack?
+    return sorted(hand) == [1, 10]
+
+
+class BlackjackEnv3(gym.Env):
+    """Simple blackjack environment
+    Blackjack is a card game where the goal is to obtain cards that sum to as
+    near as possible to 21 without going over.  They're playing against a fixed
+    dealer.
+    Face cards (Jack, Queen, King) have point value 10.
+    Aces can either count as 11 or 1, and it's called 'usable' at 11.
+    This game is placed with an infinite deck (or with replacement).
+    The game starts with dealer having one face up and one face down card, while
+    player having two face up cards. (Virtually for all Blackjack games today).
+    The player can request additional cards (hit=1) until they decide to stop
+    (stick=0) or exceed 21 (bust).
+    After the player sticks, the dealer reveals their facedown card, and draws
+    until their sum is 17 or greater.  If the dealer goes bust the player wins.
+    If neither player nor dealer busts, the outcome (win, lose, draw) is
+    decided by whose sum is closer to 21.  The reward for winning is +1,
+    drawing is 0, and losing is -1.
+    The observation of a 3-tuple of: the players current sum,
+    the dealer's one showing card (1-10 where 1 is ace),
+    and whether or not the player holds a usable ace (0 or 1).
+    This environment corresponds to the version of the blackjack problem
+    described in Example 5.1 in Reinforcement Learning: An Introduction
+    by Sutton and Barto.
+    http://incompleteideas.net/book/the-book-2nd.html
+    """
+    def __init__(self, natural=False):
+        self.action_space = spaces.Discrete(3)
+        self.observation_space = spaces.Tuple((
+            spaces.Discrete(32),
+            spaces.Discrete(11),
+            spaces.Discrete(2),
+            spaces.Discrete(21)))
+        self.seed()
+        
+        self.shuffle_deck()
+
+        # Flag to payout 1.5 on a "natural" blackjack win, like casino rules
+        # Ref: http://www.bicyclecards.com/how-to-play/blackjack/
+        self.natural = natural
+
+    def seed(self, seed=None):
+        self.np_random, seed = seeding.np_random(seed)
+        return [seed]
+    
+    def shuffle_deck(self):
+        self.deck = global_deck.copy()
+        self.np_random.shuffle(self.deck)
+        self.count = 0  # - ноль из системы "Плюс-Минус"
+        
+    def plus_minus(self, card):
+        if card >= 2 and card <= 6:
+            self.count += 1
+        elif card == 1 or card == 10:
+            self.count -= 1
+    
+    def real_count(self):
+        cnt = round(self.count * 52 / len(self.deck))
+        return min(max(cnt, -10), 10) + 10
+    
+    def draw_card(self):
+        card = self.deck[-1]
+        del self.deck[-1]
+        if len(self.deck) < 15:
+            self.shuffle_deck()
+        return card
+
+    def draw_hand(self):
+        return [self.draw_card(), self.draw_card()]
+
+    def step(self, action):
+        assert self.action_space.contains(action)
+        if action == 2:
+            card = self.draw_card()
+            self.plus_minus(card)
+            self.player.append(card)
+            done = True
+            self.plus_minus(self.dealer[1])
+            while sum_hand(self.dealer) < 17:
+                card = self.draw_card()
+                self.plus_minus(card)
+                self.dealer.append(card)
+            reward = cmp(score(self.player), score(self.dealer))
+            if self.natural and is_natural(self.player) and reward == 1.:
+                reward = 1.5
+            reward *= 2
+        elif action == 1:  # hit: add a card to players hand and return
+            card = self.draw_card()
+            self.plus_minus(card)
+            self.player.append(card)
+            if is_bust(self.player):
+                done = True
+                reward = -1.
+            else:
+                done = False
+                reward = 0.
+        else:  # stick: play out the dealers hand, and score
+            done = True
+            self.plus_minus(self.dealer[1])
+            while sum_hand(self.dealer) < 17:
+                card = self.draw_card()
+                self.plus_minus(card)
+                self.dealer.append(card)
+            reward = cmp(score(self.player), score(self.dealer))
+            if self.natural and is_natural(self.player) and reward == 1.:
+                reward = 1.5
+        return self._get_obs(), reward, done, {}
+
+    def _get_obs(self):
+        return (sum_hand(self.player), self.dealer[0], usable_ace(self.player), self.real_count())
+
+    def reset(self):
+        self.dealer = self.draw_hand()
+        self.plus_minus(self.dealer[0])
+        self.player = self.draw_hand()
+        for card in self.player:
+            self.plus_minus(card)
+        return self._get_obs()
